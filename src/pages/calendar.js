f2b17ccff6ee5920e3b9aaa1fb97b7d918e01282
mark sauer-utley <mark.utley.713@gmail.com>
2019-08-18 13:28:53 -0400

    feature(netlify-cms): new json structure

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -5,9 +5,28 @@ import SEO from "../components/seo"
 import Year from "../components/Year"
 
 export default ({ data }) => {
-	const years = data.allCalendarJson.nodes
+	const years = data.allConcertsJson.edges.reduce((acc, edge) => {
+		const year = edge.node.fields.year
+		const { location, program, date } = edge.node
 
-	const mappedYears = years.map(year => <Year year={year} key={year.year} />)
+		if (!acc[year]) acc[year] = { year, concerts: [] }
+
+		// create a new array with the current concert
+		const updatedConcerts = [...acc[year].concerts, { location, program, date }]
+
+		// sort it by date in descending order
+		const sortedConcerts = updatedConcerts.sort((a, b) => {
+			return new Date(`${b.date}, ${year}`) - new Date(`${a.date}, ${year}`)
+		})
+
+		// reassign the concerts array to the sorted concerts
+		acc[year].concerts = sortedConcerts
+		return acc
+	}, {})
+
+	const mappedYears = Object.keys(years)
+		.sort((a, b) => b - a)
+		.map(year => <Year year={years[year]} key={year} />)
 
 	return (
 		<Layout>
@@ -19,21 +38,23 @@ export default ({ data }) => {
 
 export const query = graphql`
 	query {
-		allCalendarJson {
-			nodes {
-				year
-				concerts {
-					date
-					location {
-						city
-						support
-						venue
+		allConcertsJson {
+			edges {
+				node {
+					fields {
+						year
 					}
 					program {
 						composer
 						description
 						title
 					}
+					location {
+						city
+						support
+						venue
+					}
+					date
 				}
 			}
 		}
