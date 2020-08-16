d1987e2440e3adb9166a7c5f9e06078be3a70440
mark sauer-utley <mark.utley.713@gmail.com>
2020-08-16 10:46:09 -0400

    fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,25 +3,21 @@ import PropTypes from "prop-types"
 import BackgroundImage from "./BackgroundImage"
 import message from "./message.js"
 
-const Layout = ({ children, isHomePage }) => {
+const Layout = ({ children }) => {
 	useEffect(() => console.log(message), [])
 
-	const foreGround = (
-		<div
-			style={{
-				margin: `0 auto`,
-				paddingTop: 0,
-			}}
-		>
-			{children}
-		</div>
+	return (
+		<BackgroundImage>
+			<div
+				style={{
+					margin: `0 auto`,
+					paddingTop: 0,
+				}}
+			>
+				{children}
+			</div>
+		</BackgroundImage>
 	)
-
-	const withBackgroundImage = <BackgroundImage>{foreGround}</BackgroundImage>
-
-	if (isHomePage) return withBackgroundImage
-
-	return foreGround
 }
 
 Layout.propTypes = {
