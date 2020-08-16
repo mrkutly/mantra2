81cd9f0ce7610680fcd6d4e843de4935e5bc7278
mark sauer-utley <mark.utley.713@gmail.com>
2020-08-16 12:13:36 -0400

    add welch and siegel operas

--- src/components/media/VideoPlayer.tsx
+++ src/components/media/VideoPlayer.tsx
@@ -10,6 +10,7 @@ const VideoPlayer = ({ video }: PlayerProps) => (
 			width="100%"
 			height="100%"
 			src={`${video.url}`}
+			style={{ background: "black" }}
 			frameBorder="0"
 			allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
 			allowFullScreen
