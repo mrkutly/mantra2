67ee40c761e4c61aa088cf21276b03a07ba3a372
Mark Utley <markutley@Marks-MacBook-Pro.local>
2020-11-29 11:23:57 -0400

    fix eslint errors

--- src/components/mantraYouth/Layout.js
+++ src/components/mantraYouth/Layout.js
@@ -1,114 +1,114 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import styled from 'styled-components'
-import { borderBottomColorChange } from '../styles/animations'
+// import React from 'react'
+// import { Link } from 'gatsby'
+// import styled from 'styled-components'
+// import { borderBottomColorChange } from '../styles/animations'
 
-const Layout = props => (
-	<Container>
-		<NavBar>
-			<Band>
-				<Link
-					to="/mantrayouth/"
-					style={{ textDecoration: 'none' }}
-					activeClassName="active"
-					partiallyActive={false}
-				>
-					| mantra youth percussion |
-				</Link>
-			</Band>
-			<BandMember>
-				<Link
-					to="/mantrayouth/necessary-noise-festival/"
-					style={{ textDecoration: 'none' }}
-					activeClassName="active"
-				>
-					necessary noise festival
-				</Link>
-			</BandMember>
-			<Divider>|</Divider>
-		</NavBar>
-		<Content>{props.children}</Content>
-	</Container>
-)
+// const Layout = props => (
+// 	<Container>
+// 		<NavBar>
+// 			<Band>
+// 				<Link
+// 					to="/mantrayouth/"
+// 					style={{ textDecoration: 'none' }}
+// 					activeClassName="active"
+// 					partiallyActive={false}
+// 				>
+// 					| mantra youth percussion |
+// 				</Link>
+// 			</Band>
+// 			<BandMember>
+// 				<Link
+// 					to="/mantrayouth/necessary-noise-festival/"
+// 					style={{ textDecoration: 'none' }}
+// 					activeClassName="active"
+// 				>
+// 					necessary noise festival
+// 				</Link>
+// 			</BandMember>
+// 			<Divider>|</Divider>
+// 		</NavBar>
+// 		<Content>{props.children}</Content>
+// 	</Container>
+// )
 
-const Container = styled.div`
-	max-width: 1300px;
-	margin: 70px auto;
-	display: grid;
-	grid-template-rows: 3vh 10vh 5fr;
+// const Container = styled.div`
+// 	max-width: 1300px;
+// 	margin: 70px auto;
+// 	display: grid;
+// 	grid-template-rows: 3vh 10vh 5fr;
 
-	@media (max-width: 900px) {
-		max-width: 1300px;
-		margin: 50px auto;
-		display: flex;
-		flex-direction: column;
-	}
-`
+// 	@media (max-width: 900px) {
+// 		max-width: 1300px;
+// 		margin: 50px auto;
+// 		display: flex;
+// 		flex-direction: column;
+// 	}
+// `
 
-const NavBar = styled.div`
-	line-height: 3em;
-	grid-row-start: 2;
+// const NavBar = styled.div`
+// 	line-height: 3em;
+// 	grid-row-start: 2;
 
-	@media (max-width: 900px) {
-		max-width: 360px;
-		margin: 30px auto;
-		font-size: 0.8em;
-	}
+// 	@media (max-width: 900px) {
+// 		max-width: 360px;
+// 		margin: 30px auto;
+// 		font-size: 0.8em;
+// 	}
 
-	a {
-		color: black;
-	}
-`
-const Content = styled.div`
-	grid-row-start: 3;
-`
+// 	a {
+// 		color: black;
+// 	}
+// `
+// const Content = styled.div`
+// 	grid-row-start: 3;
+// `
 
-const Band = styled.span`
-	display: inline-block;
-	font-weight: bold;
-	font-size: 2em;
-	cursor: pointer;
+// const Band = styled.span`
+// 	display: inline-block;
+// 	font-weight: bold;
+// 	font-size: 2em;
+// 	cursor: pointer;
 
-	@media screen and (max-width: 900px) {
-		font-size: 1.8em;
-		display: block;
-		text-align: center;
-	}
+// 	@media screen and (max-width: 900px) {
+// 		font-size: 1.8em;
+// 		display: block;
+// 		text-align: center;
+// 	}
 
-	a {
-		display: inline-block;
-	}
-	a.active,
-	a:hover {
-		animation: ${borderBottomColorChange} 2.5s linear infinite;
-	}
-`
+// 	a {
+// 		display: inline-block;
+// 	}
+// 	a.active,
+// 	a:hover {
+// 		animation: ${borderBottomColorChange} 2.5s linear infinite;
+// 	}
+// `
 
-const BandMember = styled.span`
-	display: inline-block;
-	margin: 0 20px;
-	font-weight: 200;
-	letter-spacing: 2px;
-	font-size: 1.5em;
-	cursor: pointer;
+// const BandMember = styled.span`
+// 	display: inline-block;
+// 	margin: 0 20px;
+// 	font-weight: 200;
+// 	letter-spacing: 2px;
+// 	font-size: 1.5em;
+// 	cursor: pointer;
 
-	a {
-		display: inline-block;
-	}
+// 	a {
+// 		display: inline-block;
+// 	}
 
-	a.active,
-	a:hover {
-		font-weight: 550;
-		animation: ${borderBottomColorChange} 2.5s linear infinite;
-	}
-`
+// 	a.active,
+// 	a:hover {
+// 		font-weight: 550;
+// 		animation: ${borderBottomColorChange} 2.5s linear infinite;
+// 	}
+// `
 
-const Divider = styled.span`
-	font-size: 1.5em;
+// const Divider = styled.span`
+// 	font-size: 1.5em;
 
-	@media (max-width: 900px) {
-		display: none;
-	}
-`
+// 	@media (max-width: 900px) {
+// 		display: none;
+// 	}
+// `
 
-export default Layout
+// export default Layout
--- src/components/mantraYouth/NecessaryNoise.js
+++ src/components/mantraYouth/NecessaryNoise.js
@@ -1,127 +1,125 @@
-import React from "react"
-import styled from "styled-components"
-import Img from "gatsby-image"
-import colors from "../styles/colors"
-import { ImageContainer } from "../about/About"
+// import React from 'react'
+// import styled from 'styled-components'
+// import Img from 'gatsby-image'
+// import colors from '../styles/colors'
+// import { ImageContainer } from '../about/About'
 
-const NecessaryNoise = props => {
-	return (
-		<Container>
-			<ImageContainer>
-				<Img
-					fluid={props.image}
-					alt="Mantra Youth Percussion playing Angélica Negrón's Count to Five"
-				/>
-			</ImageContainer>
-			<p>
-				Mantra Percussion's Necessary Noise Festival (Now in its 6th season) is
-				one of the largest percussion events on the east coast. The festival
-				provides an outlet for scholastic, collegiate, and professional
-				musicians and ensembles to share their talents in a variety of mediums
-				while promoting the art of contemporary percussion performance to the
-				greater community. The 2019 edition of NNF will took place at Rahway
-				High School in NJ February 9th from 3-9pm. Featured ensembles this year
-				included Mantra Percussion, Mantra Youth Percussion, Manhattan School of
-				Music Percussion Ensemble, NYU Percussion Ensemble, Bard College
-				Percussion Ensemble, William Paterson University Percussion Ensemble,
-				and Juilliard Pre-College Percussion Ensemble.
-			</p>
-			<div style={{ margin: "20vh 0" }}>
-				<p>Past festivals have featured performances and workshops from:</p>
-				<ListContainer tabIndex={0}>
-					<ul>
-						<li>Stony Brook University</li>
-						<li>Rutgers University</li>
-						<li>NJCU</li>
-						<li>Bard College</li>
-						<li>William Paterson University</li>
-						<li>New York University</li>
-						<li>Juilliard</li>
-						<li>Kean University</li>
-						<li>SUNY Purchase</li>
-						<li>Manhattan School of Music</li>
-					</ul>
-					<ul>
-						<li>Xylopholks</li>
-						<li>The Cadets</li>
-						<li>Anaglyphs</li>
-						<li>Marateck</li>
-						<li>Rahway High School</li>
-						<li>Rahway Academy</li>
-						<li>Rahway Elementary School</li>
-						<li>Rahway Elementary School</li>
-						<li>Verona High School</li>
-						<li>Montville High School</li>
-					</ul>
-					<ul>
-						<li>Lazaar Middle School</li>
-						<li>Ridgewood High School</li>
-						<li>Randolph High School</li>
-						<li>West Essex High School</li>
-						<li>Metuchen High School</li>
-						<li>West Orange High School</li>
-						<li>Rutgers Youth Percussion Ensemble</li>
-						<li>Special Music School</li>
-						<li>Ramsey High School</li>
-					</ul>
-				</ListContainer>
-			</div>
-			<footer>
-				Interested ensembles should contact Festival Director Joe Bergen at{" "}
-				<a href="mailto:joe@mantrapercussion.org">joe@mantrapercussion.org</a>{" "}
-				for more information.
-			</footer>
-		</Container>
-	)
-}
+// const NecessaryNoise = props => (
+// 	<Container>
+// 		<ImageContainer>
+// 			<Img
+// 				fluid={props.image}
+// 				alt="Mantra Youth Percussion playing Angélica Negrón's Count to Five"
+// 			/>
+// 		</ImageContainer>
+// 		<p>
+// 			Mantra Percussion's Necessary Noise Festival (Now in its 6th season) is
+// 			one of the largest percussion events on the east coast. The festival
+// 			provides an outlet for scholastic, collegiate, and professional musicians
+// 			and ensembles to share their talents in a variety of mediums while
+// 			promoting the art of contemporary percussion performance to the greater
+// 			community. The 2019 edition of NNF will took place at Rahway High School
+// 			in NJ February 9th from 3-9pm. Featured ensembles this year included
+// 			Mantra Percussion, Mantra Youth Percussion, Manhattan School of Music
+// 			Percussion Ensemble, NYU Percussion Ensemble, Bard College Percussion
+// 			Ensemble, William Paterson University Percussion Ensemble, and Juilliard
+// 			Pre-College Percussion Ensemble.
+// 		</p>
+// 		<div style={{ margin: '20vh 0' }}>
+// 			<p>Past festivals have featured performances and workshops from:</p>
+// 			<ListContainer tabIndex={0}>
+// 				<ul>
+// 					<li>Stony Brook University</li>
+// 					<li>Rutgers University</li>
+// 					<li>NJCU</li>
+// 					<li>Bard College</li>
+// 					<li>William Paterson University</li>
+// 					<li>New York University</li>
+// 					<li>Juilliard</li>
+// 					<li>Kean University</li>
+// 					<li>SUNY Purchase</li>
+// 					<li>Manhattan School of Music</li>
+// 				</ul>
+// 				<ul>
+// 					<li>Xylopholks</li>
+// 					<li>The Cadets</li>
+// 					<li>Anaglyphs</li>
+// 					<li>Marateck</li>
+// 					<li>Rahway High School</li>
+// 					<li>Rahway Academy</li>
+// 					<li>Rahway Elementary School</li>
+// 					<li>Rahway Elementary School</li>
+// 					<li>Verona High School</li>
+// 					<li>Montville High School</li>
+// 				</ul>
+// 				<ul>
+// 					<li>Lazaar Middle School</li>
+// 					<li>Ridgewood High School</li>
+// 					<li>Randolph High School</li>
+// 					<li>West Essex High School</li>
+// 					<li>Metuchen High School</li>
+// 					<li>West Orange High School</li>
+// 					<li>Rutgers Youth Percussion Ensemble</li>
+// 					<li>Special Music School</li>
+// 					<li>Ramsey High School</li>
+// 				</ul>
+// 			</ListContainer>
+// 		</div>
+// 		<footer>
+// 			Interested ensembles should contact Festival Director Joe Bergen at{' '}
+// 			<a href="mailto:joe@mantrapercussion.org">joe@mantrapercussion.org</a> for
+// 			more information.
+// 		</footer>
+// 	</Container>
+// )
 
-const Container = styled.div`
-	p,
-	ul {
-		line-height: 1.6;
-		letter-spacing: 1.1px;
-		font-size: 1.3rem;
-		text-shadow: 1px 1px lightblue;
-		font-weight: 300;
-	}
+// const Container = styled.div`
+// 	p,
+// 	ul {
+// 		line-height: 1.6;
+// 		letter-spacing: 1.1px;
+// 		font-size: 1.3rem;
+// 		text-shadow: 1px 1px lightblue;
+// 		font-weight: 300;
+// 	}
 
-	ul {
-		list-style: circle;
-	}
+// 	ul {
+// 		list-style: circle;
+// 	}
 
-	footer {
-		margin: 10vh 0 4vh;
-		font-size: 1.1em;
-		text-align: center;
-		font-weight: 600;
-	}
+// 	footer {
+// 		margin: 10vh 0 4vh;
+// 		font-size: 1.1em;
+// 		text-align: center;
+// 		font-weight: 600;
+// 	}
 
-	a {
-		color: ${colors.pink};
-		font-weight: 700;
+// 	a {
+// 		color: ${colors.pink};
+// 		font-weight: 700;
 
-		:hover {
-			color: ${colors.green};
-		}
-	}
-`
+// 		:hover {
+// 			color: ${colors.green};
+// 		}
+// 	}
+// `
 
-const ListContainer = styled.div`
-	display: grid;
-	grid-template-columns: 5fr 5fr 5fr;
-	ul {
-		list-style: circle;
-		font-weight: 550;
-	}
+// const ListContainer = styled.div`
+// 	display: grid;
+// 	grid-template-columns: 5fr 5fr 5fr;
+// 	ul {
+// 		list-style: circle;
+// 		font-weight: 550;
+// 	}
 
-	@media (max-width: 500px) {
-		display: flex;
-		flex-direction: column;
-		ul {
-			margin: 0 auto;
-			max-width: 259px;
-		}
-	}
-`
+// 	@media (max-width: 500px) {
+// 		display: flex;
+// 		flex-direction: column;
+// 		ul {
+// 			margin: 0 auto;
+// 			max-width: 259px;
+// 		}
+// 	}
+// `
 
-export default NecessaryNoise
+// export default NecessaryNoise
